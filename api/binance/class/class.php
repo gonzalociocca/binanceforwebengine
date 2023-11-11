5eaebbb1df2c191b675ef71a157ec0f7869eaf73
gonzalociocca <112786087+gonzalociocca@users.noreply.github.com>
2023-11-10 22:41:49 -0300

    Add files via upload

---
+++ api/binance/class/class.php
@@ -0,0 +1,120 @@
+//==================================================================================================================================================================================================================================================================================================
+//Binance Module for WebEngine
+//CopyRight belongs to Gonzalo Ciocca & ImmortalMu.net
+//Free Resource on https://github.com/gonzalociocca/binanceforwebengine
+//This resource for automatic payments is not free, it is $5 from the copyright holder.
+//WhatsApp : +54 0343-570-4950 - Gon
+//==================================================================================================================================================================================================================================================================================================
+<?php
+            define('access', 'api');
+            require $_SERVER['DOCUMENT_ROOT'].'/includes/webengine.php';
+
+            
+  class access_db{
+    
+
+
+            public function checkMcDbStatus(){
+                 $conn = Connection::Database('MuOnline');
+                 $check = $conn->query_fetch("SELECT * FROM WEBENGINE_BINANCE_TRANSACTIONS'");
+                 if(!$check){
+                                $conn->query("create table WEBENGINE_BINANCE_TRANSACTIONS (
+                                    id int IDENTITY(1,1) PRIMARY KEY,
+                                    userID varchar(50) NULL,
+									creditType varchar(50) NULL,
+									creditAmount varchar(50) NULL,
+									fee varchar(50) NULL,
+									feeCurrency varchar(50) NULL,
+									transactionId varchar(50) NULL,
+									descmsg varchar(50) NULL,
+									statusmsg varchar(50) NULL
+                            );");
+                    return true;
+                  }
+            }
+
+
+
+            public function checkDbId($transactionId){
+                $conn = Connection::Database('MuOnline');
+                $check = $conn->query_fetch("SELECT transactionId FROM WEBENGINE_BINANCE_TRANSACTIONS WHERE transactionId = '$transactionId'");
+                if(!$check){
+                    return false;
+                }else{
+                    return true;
+                }
+            }
+
+
+            public function userID($userMu){
+                $conn = Connection::Database('MuOnline');
+                $check = $conn->query_fetch("SELECT memb_guid FROM MEMB_INFO WHERE memb___id = '$userMu'");
+                if(!$check){
+                    return $check;
+                }else{
+                    return $check;
+                }
+            }
+            
+            public function checkDbStatus($transactionId){
+                $conn = Connection::Database('MuOnline');
+                $check = $conn->query_fetch("SELECT status FROM WEBENGINE_BINANCE_TRANSACTIONS WHERE transactionId = '$transactionId'");
+                foreach($check as $result){
+                    $status = $result['status'];
+                }
+                if($status == "PAY_SUCCESS" || $status == "PAY_CLOSED"){ // test mode
+                    return true;
+                }else{
+                    return false;
+                }
+            }       
+
+            public function success($userID, $Credits){
+				$fp = fopen('locked.txt','a+');
+				if(flock($fp,LOCK_EX)){
+                try {
+                    # user id
+                    if(!Validator::UnsignedNumber($userID)) throw new Exception("invalid userid");
+                    // common class
+                    $common = new common();
+                    $accountInfo = $common->accountInformation($userID);
+                    if(!is_array($accountInfo)) throw new Exception("invalid account");
+                    $creditSystem = new CreditSystem();
+                    $creditSystem->setConfigId(mconfig('credit_config'));
+                    $configSettings = $creditSystem->showConfigs(true);
+                    switch($configSettings['config_user_col_id']) {
+                        case 'userid':
+                            $creditSystem->setIdentifier($accountInfo[_CLMN_MEMBID_]);
+                            break;
+                        case 'username':
+                            $creditSystem->setIdentifier($accountInfo[_CLMN_USERNM_]);
+                            break;
+                        case 'email':
+                            $creditSystem->setIdentifier($accountInfo[_CLMN_EMAIL_]);
+                            break;
+                        default:
+                            throw new Exception("invalid identifier");
+                    }
+                    $creditSystem->addCredits($Credits);
+                } catch (Exception $ex) {// someone has to worry about this.
+                }
+				flock($fp,LOCK_UN);
+				}
+				fclose($fp);
+            }    
+
+            public function registerPayDb($data){
+                                       
+                //REGISTRAMOS EN LA BASE DE DATOS MCPAGO LOS DATOS
+                $conn = Connection::Database('MuOnline');
+              
+                $query = "INSERT into WEBENGINE_BINANCE_TRANSACTIONS (userID,creditType,creditAmount,fee,feeCurrency,transactionId,descmsg,statusmsg) "
+                ."VALUES "  
+			    . "(:userID,:creditType,:creditAmount,:fee,:feeCurrency,:transactionId,:descmsg,:statusmsg)";
+
+                $saveConfig = $conn->query($query, $data);
+            }
+ 
+    }
+
+?>
---
+++ api/binance/success.php
@@ -0,0 +1,132 @@
+//==================================================================================================================================================================================================================================================================================================
+//Binance Module for WebEngine
+//CopyRight belongs to Gonzalo Ciocca & ImmortalMu.net
+//Free Resource on https://github.com/gonzalociocca/binanceforwebengine
+//This resource for automatic payments is not free, it is $5 from the copyright holder.
+//WhatsApp : +54 0343-570-4950 - Gon
+//==================================================================================================================================================================================================================================================================================================
+
+<?php
+   header("Access-Control-Allow-Origin: *");
+   header("Access-Control-Allow-Headers: *");
+   header("Access-Control-Allow-Methods: *");
+   header("Access-Control-Expose-Headers: Content-Length, Content-Range");
+   http_response_code(200);
+    require 'class/class.php'; 
+    loadModuleConfigs('donation.binance');
+    
+    $raw_post_data = file_get_contents('php://input');
+    $json_string = json_decode($raw_post_data, true);
+    $headers=apache_request_headers();
+	$nonce = "";
+	$timestampx;
+	$signaturex;
+	foreach ($headers as $header => $value){
+		if($header=="binancepay-signature"){
+			$signaturex=$value;
+		}
+		if($header=="binancepay-timestamp"){
+			$timestampx=$value;
+		}
+		if($header=="binancepay-nonce"){
+			$nonce=$value;
+		}
+	}
+	$status = $json_string['bizStatus'];
+	$decodedSignature = base64_decode ( $signaturex );
+	$headerx=$headers;
+	$payloadx = $timestampx . "\n" . $nonce . "\n" . $raw_post_data . "\n";
+	
+	if($status == "PAY_CLOSED" || $status == "PAY_FAIL" || $status == "PAY_SUCCESS"){
+	//Request Public Key
+	$api_key=mconfig('api_key');
+	$secret_key=mconfig('secret_key');
+	$urlapi = "https://bpay.binanceapi.com/binancepay/openapi/certificates";   
+	
+    $ch = curl_init();
+	$request = array(
+    );
+    $json_request = json_encode($request);
+	$timestamp = round(microtime(true) * 1000);
+    $payload = $timestamp."\n".$nonce."\n".$json_request."\n";
+    $signature = strtoupper(hash_hmac('SHA512',$payload,$secret_key));
+    $headers = array();
+    $headers[] = "Content-Type: application/json";
+    $headers[] = "binancepay-timestamp: $timestamp";
+    $headers[] = "binancepay-nonce: $nonce";
+    $headers[] = "binancepay-certificate-sn: $api_key";
+    $headers[] = "binancepay-signature: $signature";
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_URL, $urlapi);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json_request);
+
+    $result = curl_exec($ch);
+    if (curl_errno($ch)) { echo 'Error:' . curl_error($ch); }
+    curl_close ($ch);
+	
+	//Verify Signature
+	$publicKey = json_decode($result,true)['data']['0']['certPublic'];
+	
+	
+	$verified = "".openssl_verify($payloadx, $decodedSignature, $publicKey, OPENSSL_ALGO_SHA256 )."";
+
+	if($verified=="1"){
+		//Right signature
+		if($status == "PAY_SUCCESS"){
+		$info_data=json_decode($json_string['data'],true);
+		$info_desc=$info_data['productName'];
+		$arr = explode(" ",$info_desc);
+		$info_creditamount = $arr[0];
+		$info_credittype = $arr[1];
+		$info_user = $arr[3];
+		$info_currency = $info_data['currency'];
+		$info_fee = $info_data['totalFee'];
+		$info_transactionid = $json_string['bizIdStr'];
+		$userID = $info_user;
+            $checkUserID = access_db::userID($userID);
+            foreach($checkUserID as $dat){
+                $userID = $dat['memb_guid'];
+            }
+
+            //creamos un array con toda la info
+            $data = array(
+                'userID' => $userID,
+				'creditType' => $info_credittype,
+				'creditAmount' => $info_creditamount,
+				'fee' => $info_fee,
+				'feeCurrency' => $info_currency,
+				'transactionId' => $info_transactionid,
+				'descmsg' => $info_desc,
+                'statusmsg' => $status
+            );
+
+            //Si no existe una base de datos de Binance La creamos!
+            access_db::checkMcDbStatus();
+                //BUSCAMOS EN LA DB QUE ESE info_transactionid ESTE REGISTRADO
+                $checkDbId = access_db::checkDbId($info_transactionid);
+                //BUSCAMOS EN LA DB SI ESTA REGISTRADO COMO PAGADO
+                //$checkDbStatus = access_db::checkDbStatus($info_transactionid);
+                
+                //SI NO SE ENCUENTRÒ EL ID EN LA BASE DE DATOS, REGISTRAMOS EL NUEVO PAGO EN CASHOPDATA
+                if(!$checkDbId){
+                    access_db::success($userID, (int)$info_creditamount);
+                    access_db::registerPayDb($data);
+					echo '{"returnCode":"SUCCESS","returnMessage":"Accredited"}';
+					
+                }
+	}else if($status == "PAY_CLOSED") {
+		echo '{"returnCode":"SUCCESS","returnMessage":"Closed"}';
+	}
+	//End
+	} else {
+		echo '{"returnCode":"FAIL","returnMessage":"Wrong Signature"}';
+		}
+        } else {
+			echo '{"returnCode":"FAIL","returnMessage":"Wrong Status"}';
+			}
+     
+  
+?>
